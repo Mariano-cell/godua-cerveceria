d32a2a6106e6b1f0018e771d2328b3ca75a9531e
Mariano Richards <marianorichards@gmail.com>
2026-07-06 20:49:57 -0300

    Include srcset and CSS assets in dist build

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -41,15 +41,41 @@ while IFS= read -r asset_path; do
   mkdir -p "$DIST_DIR/$(dirname "$asset_path")"
   cp "$ROOT_DIR/$asset_path" "$DIST_DIR/$asset_path"
 done < <(
-  perl -nE '
-    while (/(?:src|href)="([^"]+)"/g) {
+  perl -0777 -nE '
+    while (/(?:src|href)\s*=\s*"([^"]+)"/g) {
       my $path = $1;
-      next unless $path =~ m{^(?:\.\./)?assets/};
-      $path =~ s{^\.\./}{};
+      next unless $path =~ m{^(?:\.\./)+assets/|^assets/};
+      $path =~ s{^(?:\.\./)+}{};
       $path =~ s{[?#].*$}{};
       say $path;
     }
+
+    while (/srcset\s*=\s*"([^"]+)"/g) {
+      my $srcset = $1;
+      for my $candidate (split /\s*,\s*/, $srcset) {
+        my ($path) = split /\s+/, $candidate;
+        next unless defined $path && $path =~ m{^(?:\.\./)+assets/|^assets/};
+        $path =~ s{^(?:\.\./)+}{};
+        $path =~ s{[?#].*$}{};
+        say $path;
+      }
+    }
   ' "${PUBLIC_PAGES[@]/#/$ROOT_DIR/}" "${DETAIL_PAGES[@]/#/$ROOT_DIR/}" | sort -u
+)
+
+while IFS= read -r asset_path; do
+  mkdir -p "$DIST_DIR/$(dirname "$asset_path")"
+  cp "$ROOT_DIR/$asset_path" "$DIST_DIR/$asset_path"
+done < <(
+  find "$ROOT_DIR/css" -type f -name '*.css' -print0 | xargs -0 perl -0777 -nE '
+    while (/url\((["'"'"']?)([^)"'"'"']+)\1\)/g) {
+      my $path = $2;
+      next unless $path =~ m{^(?:\.\./)+assets/|^assets/};
+      $path =~ s{^(?:\.\./)+}{};
+      $path =~ s{[?#].*$}{};
+      say $path;
+    }
+  ' | sort -u
 )
 
 find "$DIST_DIR" -name '.DS_Store' -delete
